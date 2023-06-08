9ecbd18df9d1277423b40fa2b6d0cc87c58f161f
Arthur <1627072456@qq.com>
2023-06-08 15:46:05 +0800

    野怪生成策略

--- src/main/java/com/mcylm/coi/realm/listener/GameListener.java
+++ src/main/java/com/mcylm/coi/realm/listener/GameListener.java
@@ -5,8 +5,11 @@ import com.mcylm.coi.realm.clipboard.PlayerClipboard;
 import com.mcylm.coi.realm.enums.COIBuildingType;
 import com.mcylm.coi.realm.enums.COIGameStatus;
 import com.mcylm.coi.realm.enums.COIServerMode;
+import com.mcylm.coi.realm.tools.attack.target.impl.BuildingTarget;
 import com.mcylm.coi.realm.tools.building.COIBuilding;
+import com.mcylm.coi.realm.tools.data.metadata.BuildData;
 import com.mcylm.coi.realm.tools.team.impl.COITeam;
+import com.mcylm.coi.realm.utils.LocationUtils;
 import com.mcylm.coi.realm.utils.TeamUtils;
 import me.lucko.helper.Events;
 import net.citizensnpcs.api.CitizensAPI;
@@ -52,25 +55,51 @@ public class GameListener implements Listener {
                 event.setCancelled(true);
 
                 // 是个活得
-                if(event.getEntity().getType().equals(EntityType.ZOMBIE)
-                ){
-                    // 非CitizensAPI生成的指定上述类型的原版怪物
-                    // 创建一只普通怪
-//                    try {
-//
-//                        COITeam monsterTeam = Entry.getGame().getMonsterTeam();
-//
-//                        if(monsterTeam != null){
-//                            // 怪物阵营存在的情况下，才会生成
-//
-//                            COIBuilding monsterBase = Entry.getInstance().getBuildingManager().getBuildingTemplateByType(COIBuildingType.MONSTER_BASE);
-//                            monsterBase.setTeam(monsterTeam);
-//                            monsterBase.build(event.getLocation(),monsterTeam,false);
-//                        }
-//
-//                    } catch (Exception e) {
-//                        throw new RuntimeException(e);
-//                    }
+                try {
+
+                    // 周围30格有建筑物则不允许生成怪物小队
+                    // TODO 游戏中后期要自动调整这个数字
+                    int radius = 30;
+
+                    int maxMonsterBase = 50;
+
+                    // 生成的几率
+                    boolean result = hasChance(0.2);
+
+                    if(result){
+                        // 判断所在位置附近有没有建筑物
+                        Location clone = event.getLocation().clone();
+
+                        for (Block b : LocationUtils.selectionRadiusByDistance(clone.getBlock(), radius, radius)) {
+                            COIBuilding building = BuildData.getBuildingByBlock(b);
+                            if (building != null && building.getTeam() != TeamUtils.getMonsterTeam()) {
+
+                                // 半径范围内发现玩家小队的建筑，禁止生成
+                               return;
+                            }
+                        }
+
+                        COITeam monsterTeam = Entry.getGame().getMonsterTeam();
+
+                        if(monsterTeam != null){
+                            // 开启怪物阵营的情况下，才会生成
+
+                            if(monsterTeam.getFinishedBuildings().size() >= maxMonsterBase){
+                                // 超过最大创建数量了，禁止生成
+                                return;
+                            }
+
+                            clone.setY(clone.getY() - 1);
+                            COIBuilding monsterBase = Entry.getInstance().getBuildingManager().getBuildingTemplateByType(COIBuildingType.MONSTER_BASE);
+                            monsterBase.setTeam(monsterTeam);
+                            monsterBase.build(clone,monsterTeam,false);
+                        }
+                    }
+
+
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
 
 
@@ -78,5 +107,14 @@ public class GameListener implements Listener {
 
 
         }
+    }
+
+    /**
+     * 百分之多少的几率返回true
+     * @param chance 1 = 100%,0.1 = 10%
+     * @return
+     */
+    private boolean hasChance(double chance) {
+        return Math.random() < chance;
     }
 }
